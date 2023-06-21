744c4096411a5d8d6bfe732269351cfb7a875e6e
Analealjob <analeal.job@gmail.com>
2023-06-20 23:10:13 -0300

    terceira entrega

---
+++ main.js
@@ -0,0 +1,42 @@
+let textoA = "OLÁ MUNDO";   
+
+let textoB = "SOMOS A ACADEMY TELECOM";
+
+let frase1 = textoA +"SEJA BEM VINDO" + textoB;
+
+let unir = textoA + textoB 
+
+
+console.log(unir)
+
+
+[ 
+
+
+    {
+        curso: "Especialista " ,
+        professor: "Ana Leal",
+        turno: "Diurno", "Noturno",
+        carga: "360 "
+    }
+    
+    
+    
+    {
+        curso: "Engenharia de Redes " ,
+        professor: "Priscila do Prado",
+        turno: "Diurno", "Noturno",
+        carga: "360 "
+    }
+    
+]
+
+function Especialista (professor, turno,carga) {
+    this.professor = professor ;
+    this.turno = turno ;
+    this.carga =carga ;
+}
+
+const NovoCurso = new curso ( Inicianates , Priscila , Noturno) ;
+curso.push (NovoCurso) ;
+console.log (curso) ;
